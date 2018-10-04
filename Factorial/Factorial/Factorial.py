9af1d1c53b449ddcb823187e5572413ecd7fb8e2
Tamu Boima Fahnbulleh <22999730+tfahnbulleh@users.noreply.github.com>
2018-10-04 03:48:43 -0500

    Add files via upload

---
+++ Factorial/Factorial/Factorial.py
@@ -0,0 +1,17 @@
+
+ #compute the factorial of a number
+def computerFactorial(number):
+    if number ==0 or number == 1 or number == 2: # number is 0, 1, or 2
+       return number     
+    else: # number is greater than 2
+
+       total=(number-1) *number 
+       number=number-2
+       while number >1:
+           total=total*number
+           number=number-1
+    print(total)
+    return total
+
+
+computerFactorial(-5)
